7323c6143d4dc8ec07ced8046bf713257a40e83a
magnae2016 <magnae2016@gmail.com>
2022-06-04 12:15:00 +0900

    Update path alias @

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -11,10 +11,7 @@ const routes: Array<RouteRecordRaw> = [
     path: "/about",
     name: "about",
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: () => import("@/views/AboutView.vue"),
   },
 ];
 
