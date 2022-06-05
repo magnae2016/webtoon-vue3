f117adc8600631c05602d04aa18ef54c28052f20
magnae2016 <magnae2016@gmail.com>
2022-06-05 23:43:41 +0900

    Apply m query string to response data

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const fs = require("fs");
 
 router.get("/realTimeRankChoice", function (req, res, next) {
-  const { m, order } = req.query;
+  const { m = "list", order } = req.query;
 
   let path = "";
   if (order === "User") {
@@ -16,7 +16,7 @@ router.get("/realTimeRankChoice", function (req, res, next) {
     fs.readFile(path, "utf8", (err, data) => {
       if (err) throw err;
 
-      const body = { list: JSON.parse(data) };
+      const body = { [m]: JSON.parse(data) };
       res.json(body);
     });
   } catch (error) {
