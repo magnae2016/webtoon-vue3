ca9db1b4fc767b4442d7b8489126325ca9aab09b
magnae2016 <magnae2016@gmail.com>
2022-06-06 22:23:11 +0900

    Add View divide by router name using onBeforeRouteUpdate

--- frontend/src/views/WebtoonView/content/WebtoonSpot/index.vue
+++ frontend/src/views/WebtoonView/content/WebtoonSpot/index.vue
@@ -1,15 +1,26 @@
 <template>
-  <weekday-view></weekday-view>
-  <weekday-list-view></weekday-list-view>
+  <weekday-view v-if="routeName === 'weekday'"></weekday-view>
+  <weekday-list-view
+    v-else-if="routeName === 'weekdayList'"
+  ></weekday-list-view>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import { onBeforeRouteUpdate } from "vue-router";
 import WeekdayView from "./WeekdayView.vue";
 import WeekdayListView from "./WeekdayListView.vue";
 
 export default defineComponent({
   components: { WeekdayView, WeekdayListView },
+  setup() {
+    const routeName = ref("weekday");
+
+    onBeforeRouteUpdate((to) => {
+      routeName.value = String(to.name);
+    });
+    return { routeName };
+  },
 });
 </script>
 
