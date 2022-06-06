7ea9939f83aa4ee65cd7bf61fcd7c7e837baf5d4
magnae2016 <magnae2016@gmail.com>
2022-06-06 22:23:38 +0900

    Add data binding using monthNewWebtoon data

--- frontend/src/views/WebtoonView/content/WebtoonSpot/WeekdayView.vue
+++ frontend/src/views/WebtoonView/content/WebtoonSpot/WeekdayView.vue
@@ -2,41 +2,46 @@
   <div class="webtoon_spot2">
     <subTitle>이달의 신규 웹툰</subTitle>
     <ul>
-      <li>
+      <li v-for="item in items" :key="item.randId">
         <div class="thumb7">
-          <a href="/webtoon/detail?titleId=794423&amp;no=6">
+          <a href="#">
             <img
               src="https://shared-comic.pstatic.net/thumb/webtoon/794423/thumbnail/thumbnail_IMAG04_da93d1ce-b945-4858-935f-293b7b6e594b.jpg"
               width="218"
               height="120"
-              alt="너의 키스씬"
-              title="너의 키스씬"
+              :alt="item.titleName"
+              :title="item.titleName"
             />
             <span class="mask"></span>
           </a>
-          <span class="ico_new2">NEW</span>
+          <span v-if="item.newYN === 'Y'" class="ico_new2">NEW</span>
         </div>
-        <a
-          onclick="nclk_v2(event,'mom.title','794423','1')"
-          href="/webtoon/list?titleId=794423"
-        >
-          <strong title="너의 키스씬">너의 키스씬</strong>
+        <a href="#">
+          <strong :title="item.titleName">{{ item.titleName }}</strong>
         </a>
-        <p class="author2"><a href="#" title="조니조"> 조니조</a></p>
-        <p>
-          예능작가 이슬, 새로 담당한 예능프로그램에 최애 아이돌인 블록슛 승찬...
+        <p class="author2">
+          <a href="#" :title="item.writer">{{ item.writer }}</a>
         </p>
+        <p>{{ item.copy }}</p>
       </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
+import { useStore } from "@/store";
 import SubTitle from "@/components/ui/SubTitle.vue";
 
 export default defineComponent({
   components: { SubTitle },
+  setup() {
+    const store = useStore();
+
+    return {
+      items: computed(() => store.state.webtoon.monthNewWebtoon),
+    };
+  },
 });
 </script>
 
