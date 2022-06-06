a2c41deb1bd212b340b5ac8affb71f535bb81956
magnae2016 <magnae2016@gmail.com>
2022-06-06 22:23:49 +0900

    Add data binding using weekdayRecommendWebtoon data

--- frontend/src/views/WebtoonView/content/WebtoonSpot/WeekdayListView.vue
+++ frontend/src/views/WebtoonView/content/WebtoonSpot/WeekdayListView.vue
@@ -1,73 +1,67 @@
 <template>
-  <div>
-    <div class="webtoon_spot">
-      <subTitle
-        >일요 추천 웹툰
-        <template v-slot:TODAY><span>TODAY : 2022.06.05</span></template>
-      </subTitle>
-      <ul>
-        <li>
-          <div class="thumb">
-            <a
-              href="/webtoon/detail?titleId=776092&amp;no=49&amp;weekday=sun"
-              title="어느날 갑자기 서울은"
-              ><img
-                src="https://shared-comic.pstatic.net/thumb/webtoon/776092/thumbnail/thumbnail_IMAG04_96f47e9d-9811-4917-b973-e75704372975.jpg"
-                title="어느날 갑자기 서울은"
-                alt="어느날 갑자기 서울은"
-                width="218"
-                height="120"
-              />
-              <span class="mask"></span>
+  <div class="webtoon_spot">
+    <subTitle
+      >일요 추천 웹툰
+      <template v-slot:TODAY><span>TODAY : 2022.06.05</span></template>
+    </subTitle>
+    <ul>
+      <li v-for="item in items" :key="item.randId">
+        <div class="thumb">
+          <a href="#" :title="item.titleName"
+            ><img
+              src="https://shared-comic.pstatic.net/thumb/webtoon/776092/thumbnail/thumbnail_IMAG04_96f47e9d-9811-4917-b973-e75704372975.jpg"
+              :title="item.titleName"
+              :alt="item.titleName"
+              width="218"
+              height="120"
+            />
+            <span class="mask"></span>
+          </a>
+        </div>
+        <dl>
+          <dt>
+            <a href="#"
+              ><strong :title="item.titleName">{{ item.titleName }}</strong>
             </a>
-          </div>
-          <dl>
-            <dt>
-              <a href="/webtoon/list?titleId=776092&amp;weekday=sun"
-                ><strong title="어느날 갑자기 서울은"
-                  >어느날 갑자기 서울은</strong
-                >
-              </a>
-            </dt>
-            <dd class="desc">
-              <a
-                onclick="nclk_v2(event,'pop.num','776092','1')"
-                href="/webtoon/detail?titleId=776092&amp;no=49&amp;weekday=sun"
-                class="subtitle"
-                title="49화"
+          </dt>
+          <dd class="desc">
+            <a href="#" class="subtitle" title="49화"> 49화 </a>
+            <p>
+              <a href="#" class="author" :title="item.writer">
+                {{ item.writer }}</a
               >
-                49화
-              </a>
-              <p>
-                <a
-                  href="#"
-                  onclick="return artistAction.viewArtist('776092', this);"
-                  class="author"
-                  title="박창근"
-                >
-                  박창근</a
-                >
-              </p>
-            </dd>
-            <dd>
-              <div class="rating_type2">
-                <span class="star"><em style="width: 99.44%">평점</em></span>
-                <strong>9.94</strong>
-              </div>
-            </dd>
-          </dl>
-        </li>
-      </ul>
-    </div>
+            </p>
+          </dd>
+          <dd>
+            <div class="rating_type2">
+              <span class="star"
+                ><em :style="`width: ${item.starscoreToPercent}%`"
+                  >평점</em
+                ></span
+              >
+              <strong>{{ item.starscoreViewFormat }}</strong>
+            </div>
+          </dd>
+        </dl>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
+import { useStore } from "@/store";
 import SubTitle from "@/components/ui/SubTitle.vue";
 
 export default defineComponent({
   components: { SubTitle },
+  setup() {
+    const store = useStore();
+
+    return {
+      items: computed(() => store.state.webtoon.weekdayRecommendWebtoon),
+    };
+  },
 });
 </script>
 
