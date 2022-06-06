0a682325151bc81db0d4c391b68b9b86e4e13ff8
magnae2016 <magnae2016@gmail.com>
2022-06-06 23:15:35 +0900

    Add titleText on subTitle of WeekdayList

--- frontend/src/views/WebtoonView/content/WebtoonSpot/WeekdayListView.vue
+++ frontend/src/views/WebtoonView/content/WebtoonSpot/WeekdayListView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="webtoon_spot">
     <subTitle
-      >일요 추천 웹툰
+      >{{ titleText }} 추천 웹툰
       <template v-slot:TODAY><span>TODAY : 2022.06.05</span></template>
     </subTitle>
     <ul>
@@ -9,7 +9,7 @@
         <div class="thumb">
           <a href="#" :title="item.titleName"
             ><img
-              src="https://shared-comic.pstatic.net/thumb/webtoon/776092/thumbnail/thumbnail_IMAG04_96f47e9d-9811-4917-b973-e75704372975.jpg"
+              src="https://shared-comic.pstatic.net/thumb/webtoon/794671/thumbnail/thumbnail_IMAG04_39a756a1-3a29-41c7-815c-731a886d240e.jpg"
               :title="item.titleName"
               :alt="item.titleName"
               width="218"
@@ -50,16 +50,35 @@
 
 <script lang="ts">
 import { computed, defineComponent } from "vue";
+import { useRoute } from "vue-router";
 import { useStore } from "@/store";
 import SubTitle from "@/components/ui/SubTitle.vue";
+import { Weekday } from "@/types/webtoon";
 
 export default defineComponent({
   components: { SubTitle },
   setup() {
     const store = useStore();
+    const route = useRoute();
+
+    const weekdayTitleText = {
+      mon: "월요",
+      tue: "화요",
+      wed: "수요",
+      thu: "목요",
+      fri: "금요",
+      sat: "토요",
+      sun: "일요",
+    };
+
+    const routeWeek = computed(() => route.query.week);
+    const titleText = computed(
+      () => weekdayTitleText[routeWeek.value as Weekday]
+    );
 
     return {
       items: computed(() => store.state.webtoon.weekdayRecommendWebtoon),
+      titleText,
     };
   },
 });
--- frontend/src/views/WebtoonView/content/WebtoonSpot/WeekdayView.vue
+++ frontend/src/views/WebtoonView/content/WebtoonSpot/WeekdayView.vue
@@ -6,7 +6,7 @@
         <div class="thumb7">
           <a href="#">
             <img
-              src="https://shared-comic.pstatic.net/thumb/webtoon/794423/thumbnail/thumbnail_IMAG04_da93d1ce-b945-4858-935f-293b7b6e594b.jpg"
+              src="https://shared-comic.pstatic.net/thumb/webtoon/794161/thumbnail/thumbnail_IMAG04_3a84d478-d17f-46f6-844b-f7faaadb1675.jpg"
               width="218"
               height="120"
               :alt="item.titleName"
