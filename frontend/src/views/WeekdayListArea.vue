99387134bb78528c40ced30b3089feccfda553c9
magnae2016 <magnae2016@gmail.com>
2022-06-08 23:38:29 +0900

    Update WeekdayView using WeekdayListArea slot

---
+++ frontend/src/views/WeekdayListArea.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li v-for="webtoon in webtoons" :key="webtoon.id">
+      <slot :webtoon="webtoon"></slot>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType } from "vue";
+import { useStore } from "@/store";
+import { Weekday } from "@/types/webtoon";
+
+export default defineComponent({
+  props: {
+    week: {
+      type: String as PropType<Weekday>,
+      required: true,
+    },
+  },
+  setup(props) {
+    const store = useStore();
+    return {
+      webtoons: computed(() => store.state.webtoon.allWebtoons[props.week]),
+    };
+  },
+});
+</script>
+
+<style></style>
--- frontend/src/views/WeekdayView.vue
+++ frontend/src/views/WeekdayView.vue
@@ -17,157 +17,27 @@
     </ul>
   </div>
   <div class="list_area daily_all">
-    <div class="col col_selected">
+    <div v-for="week in Weekday" :key="week" class="col">
       <div class="col_inner">
-        <h4 class="mon"><span>월요 웹툰</span></h4>
-        <ul>
-          <li>
+        <h4 :class="week"><span>월요 웹툰</span></h4>
+        <weekday-list-area :week="week">
+          <template v-slot="slotProps">
             <div class="thumb">
               <a href="#">
                 <img
-                  src="https://shared-comic.pstatic.net/thumb/webtoon/758037/thumbnail/thumbnail_IMAG10_a2297504-4912-4c7e-a5a8-524d6fc77103.jpg"
+                  :src="`https://shared-comic.pstatic.net/thumb/webtoon/${slotProps.webtoon.titleId}/thumbnail/${slotProps.webtoon.thumbnailFilename}`"
                   width="83"
                   height="90"
-                  title="참교육"
-                  alt="참교육"
-                /><span class="mask"></span>
-                <em class="ico_updt"></em>
-              </a>
-            </div>
-            <a href="#" class="title" title="참교육">참교육</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="col">
-      <div class="col_inner">
-        <h4 class="tue"><span>화요 웹툰</span></h4>
-        <ul>
-          <li>
-            <div class="thumb">
-              <a href="#">
-                <img
-                  src="https://shared-comic.pstatic.net/thumb/webtoon/783053/thumbnail/thumbnail_IMAG10_7e1a48d2-11e0-4455-a668-fff349d8b3f3.jpg"
-                  width="83"
-                  height="90"
-                  title="김부장"
-                  alt="김부장"
-                /><span class="mask"></span>
-                <em class="ico_updt"></em>
-              </a>
-            </div>
-            <a href="#" class="title" title="김부장">김부장</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="col">
-      <div class="col_inner">
-        <h4 class="wed"><span>수요 웹툰</span></h4>
-        <ul>
-          <li>
-            <div class="thumb">
-              <a href="#">
-                <img
-                  src="https://shared-comic.pstatic.net/thumb/webtoon/769209/thumbnail/thumbnail_IMAG10_13ff2e28-686a-42de-890e-9289550978bc.jpg"
-                  width="83"
-                  height="90"
-                  title="화산귀환"
-                  alt="화산귀환"
+                  :title="slotProps.webtoon.titleName"
+                  :alt="slotProps.webtoon.titleName"
                 /><span class="mask"></span>
               </a>
             </div>
-            <a href="#" class="title" title="화산귀환">화산귀환</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="col">
-      <div class="col_inner">
-        <h4 class="thu"><span>목요 웹툰</span></h4>
-        <ul>
-          <li>
-            <div class="thumb">
-              <a href="#">
-                <img
-                  src="https://shared-comic.pstatic.net/thumb/webtoon/748105/thumbnail/thumbnail_IMAG10_becd3e24-cc09-4243-a1c9-646270f4a8db.jpg"
-                  width="83"
-                  height="90"
-                  title="독립일기"
-                  alt="독립일기"
-                /><span class="mask"></span>
-                <span class="ico_cut">컷툰</span>
-              </a>
-            </div>
-            <a href="#" class="title" title="독립일기">독립일기</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="col">
-      <div class="col_inner">
-        <h4 class="fri"><span>금요 웹툰</span></h4>
-        <ul>
-          <li>
-            <div class="thumb">
-              <a href="#">
-                <img
-                  src="https://shared-comic.pstatic.net/thumb/webtoon/641253/thumbnail/thumbnail_IMAG10_d4d1a799-d193-4e5a-9b46-6dffcead862a.jpg"
-                  width="83"
-                  height="90"
-                  title="외모지상주의"
-                  alt="외모지상주의"
-                /><span class="mask"></span>
-              </a>
-            </div>
-            <a href="#" class="title" title="외모지상주의">외모지상주의</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="col">
-      <div class="col_inner">
-        <h4 class="sat"><span>토요 웹툰</span></h4>
-        <ul>
-          <li>
-            <div class="thumb">
-              <a href="#">
-                <img
-                  src="https://shared-comic.pstatic.net/thumb/webtoon/792651/thumbnail/thumbnail_IMAG10_8c733a14-32cb-4350-8ec5-231e713cdf42.jpg"
-                  width="83"
-                  height="90"
-                  title="99강화나무몽둥이"
-                  alt="99강화나무몽둥이"
-                /><span class="mask"></span>
-              </a>
-            </div>
-            <a href="#" class="title" title="99강화나무몽둥이"
-              >99강화나무몽둥이</a
-            >
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="col">
-      <div class="col_inner">
-        <h4 class="sun"><span>일요 웹툰</span></h4>
-        <ul>
-          <li>
-            <div class="thumb">
-              <a href="#">
-                <img
-                  src="https://shared-comic.pstatic.net/thumb/webtoon/748105/thumbnail/thumbnail_IMAG10_becd3e24-cc09-4243-a1c9-646270f4a8db.jpg"
-                  width="83"
-                  height="90"
-                  title="독립일기"
-                  alt="독립일기"
-                /><span class="mask"></span>
-                <span class="ico_cut">컷툰</span>
-              </a>
-            </div>
-            <a href="#" class="title" title="독립일기">독립일기</a>
-          </li>
-        </ul>
+            <a href="#" class="title" :title="slotProps.webtoon.titleName">{{
+              slotProps.webtoon.titleName
+            }}</a>
+          </template>
+        </weekday-list-area>
       </div>
     </div>
   </div>
@@ -176,9 +46,14 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import SubTitle from "@/components/ui/SubTitle.vue";
+import WeekdayListArea from "./WeekdayListArea.vue";
+import { Weekday } from "@/types/webtoon";
 export default defineComponent({
   name: "WeekdayView",
-  components: { SubTitle },
+  components: { SubTitle, WeekdayListArea },
+  setup() {
+    return { Weekday };
+  },
 });
 </script>
 
