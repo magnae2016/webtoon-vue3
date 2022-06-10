c06a519ee22eb88129a13408d84e7f2ae996491e
magnae2016 <magnae2016@gmail.com>
2022-06-10 23:55:54 +0900

    Add Iterate over Genres enum for GenreNavTabs routes

--- frontend/src/views/WebtoonView/content/GenreNavTabs.vue
+++ frontend/src/views/WebtoonView/content/GenreNavTabs.vue
@@ -19,86 +19,28 @@
 import { computed, defineComponent } from "vue";
 import { useRoute } from "vue-router";
 import NavTabs from "@/components/ui/NavTabs.vue";
+import { Genres } from "@/types/webtoon";
 export default defineComponent({
   components: { NavTabs },
   data() {
     return {
       linkActiveClass: "on",
-      routes: [
-        {
-          to: { name: "genre", query: { genre: "episode" } },
-          genre: "episode",
-          text: "에피소드",
-        },
-        {
-          to: { name: "genre", query: { genre: "omnibus" } },
-          genre: "omnibus",
-          text: "옴니버스",
-        },
-        {
-          to: { name: "genre", query: { genre: "story" } },
-          genre: "story",
-          text: "스토리",
-          hasDivider: true,
-        },
-        {
-          to: { name: "genre", query: { genre: "daily" } },
-          genre: "daily",
-          text: "일상",
-        },
-        {
-          to: { name: "genre", query: { genre: "comic" } },
-          genre: "comic",
-          text: "개그",
-        },
-        {
-          to: { name: "genre", query: { genre: "fantasy" } },
-          genre: "fantasy",
-          text: "판타지",
-        },
-        {
-          to: { name: "genre", query: { genre: "action" } },
-          genre: "action",
-          text: "액션",
-        },
-        {
-          to: { name: "genre", query: { genre: "drama" } },
-          genre: "drama",
-          text: "드라마",
-        },
-        {
-          to: { name: "genre", query: { genre: "pure" } },
-          genre: "pure",
-          text: "순정",
-        },
-        {
-          to: { name: "genre", query: { genre: "sensibility" } },
-          genre: "sensibility",
-          text: "감성",
-        },
-        {
-          to: { name: "genre", query: { genre: "thrill" } },
-          genre: "thrill",
-          text: "스릴러",
-        },
-        {
-          to: { name: "genre", query: { genre: "historical" } },
-          genre: "historical",
-          text: "무협/사극",
-        },
-        {
-          to: { name: "genre", query: { genre: "sports" } },
-          genre: "sports",
-          text: "스포츠",
-        },
-      ],
     };
   },
   setup() {
     const route = useRoute();
     const genre = computed(() => route.query.genre || "episode");
+    const routes = [];
+    for (const genre in Genres) {
+      const text: Genres = Genres[genre as keyof typeof Genres];
+      routes.push({
+        to: { name: "genre", query: { genre } },
+        genre,
+        text,
+      });
+    }
 
-    return { genre };
+    return { genre, routes };
   },
 });
 </script>
